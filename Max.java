a30e4845044a048150a21be6b1733a12771ad286
Preetgabani30 <109951320+Preetgabani30@users.noreply.github.com>
2022-07-25 14:13:47 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Max
+{
+	public static void main(String args[])
+	{
+		int a,b,c,max;
+		Scanner aa=new Scanner(System.in);
+		System.out.println("Enter three number");
+		a=aa.nextInt();
+		b=aa.nextInt();
+		c=aa.nextInt();
+		max=(a>b?((a>c)?a:c):((b>c)?b:c));
+		System.out.println(max+" is Max");
+	}
+}
